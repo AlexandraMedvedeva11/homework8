0017e7ac4d06814b1100f17424b9cef8a559be5c
Alexandra Medvedeva <alex.voitova@gmail.com>
2023-10-22 16:38:46 +0300

    Initial Commit

---
+++ Homework8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework8
+//
+//  Created by  Medvedeva Alexandra on 22.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
