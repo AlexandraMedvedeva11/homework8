118021342ee325c11c65245b0fc37a1edb6e2762
Alexandra Medvedeva <alex.voitova@gmail.com>
2023-10-24 16:23:23 +0300

    -

--- Homework8/ViewController.swift
+++ Homework8/ViewController.swift
@@ -7,13 +7,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class Menu {
+    var type: String = ""
+    var coast: Int = 0
+    func food () {
     }
-
-
 }
+
+class Snacks: Menu {
+    override init () {
+        super.init()
+        type = "Chips"
+    }
+    override func food () {
+        print (type, coast)
+    }
+}
+
+class Dishes: Menu {
+    override init () {
+        super.init()
+        type = "Burger"
+    }
+    override func food () {
+        print (type, coast)
+    }
+}
+
+class Drinks: Menu {
+    override init () {
+        super.init()
+        type = "Cola"
+    }
+    override func food () {
+        print (type, coast)
+    }
+}
+
+class Desert: Menu {
+    override init () {
+        super.init()
+        type = "IceCream"
+    }
+    override func food () {
+        print (type, coast)
+    }
+}
+
+let Chips = Snacks()
+Chips.coast = 7
+
+let Burger = Dishes()
+Burger.coast = 15
+
+let Cola = Drinks()
+Drinks.coast = 5
+
+let IceCream = Desert()
+IceCream.coast = 9
+
+
+
+
 
