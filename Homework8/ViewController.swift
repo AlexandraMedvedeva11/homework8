873363abc3df54ebe9ab11a78d4b5df83eb19501
Alexandra Medvedeva <alex.voitova@gmail.com>
2023-10-29 16:28:42 +0300

    -

--- Homework8/ViewController.swift
+++ Homework8/ViewController.swift
@@ -8,6 +8,46 @@
 import UIKit
 
 class Menu {
+    
+    var numberFromScreen: Double = 0
+    var firstNum: Double = 0
+    var mathSing: Bool = false
+    var operation: Double = 0
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func digits(_ sender: UIButton) {
+        if mathSing == true {
+            result.text = String(sender.tag)
+            mathSing = false
+        }
+        else {
+            
+            result.text = result.text! + String(sender.tag)
+        }
+        numberFromScreen = Double(result.text!)!
+    }
+    
+    @IBAction func operations(_ sender: UIButton) {
+        if result.text! == "" && sender.tag != 1 {
+            firstNum = Double(result.text!)!
+            
+            if sender.tag == 1 {
+                result.text = "+"
+            } else if sender.tag == 2 {
+                if operation == 1 {
+                    result.text = String (firstNum + numberFromScreen)
+                } else if sender.tag == 0 {
+                    result.text = ""
+                    firstNum = 0
+                    numberFromScreen = 0
+                    operation = 0
+                }
+            }
+        }
+        
+    }
+    
     init(snacks: Position, dishes: Position, drinks: Position, desert: Position) {
         self.snacks = snacks
         self.dishes = dishes
@@ -69,7 +109,7 @@ class Menu {
 func dosmth () {
     
     func posit (position1: Position) {
-        print(position1)
+        print(position1.type, position1.coast)
     }
     
     let Chips = Snacks()
